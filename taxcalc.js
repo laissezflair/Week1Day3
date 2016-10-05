a9c4bd9b31ac3fa7222c7c554f33f38f9672968d
Adam Smith <prokosch@gmail.com>
2016-10-05 20:24:18 +0000

    Added undefined function

---
+++ taxcalc.js
@@ -0,0 +1,18 @@
+function calculateSalesTax(salesData, taxRates) {
+  // Implement your code here
+}
+
+var results = salesTaxReport(companySalesData, salesTaxRates);
+
+/* Expected Results:
+{
+  Telus: {
+    totalSales: 1300
+    totalTaxes: 144
+  },
+  Bombardier: {
+    totalSales: 800,
+    totalTaxes: 40
+  }
+}
+*/
